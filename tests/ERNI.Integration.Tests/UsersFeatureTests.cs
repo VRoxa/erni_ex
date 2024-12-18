bca59f6357dc4fd14d2e69e68e0e71fd4f1ff05f
Víctor de la Rocha <victor.roxa.r@gmail.com>
2024-12-18 18:43:42 +0100

    Organize solution folders
    Move test projects to tests folder

--- tests/ERNI.Integration.Tests/UsersFeatureTests.cs
+++ tests/ERNI.Integration.Tests/UsersFeatureTests.cs
@@ -38,13 +38,6 @@ public class UsersFeatureTests : IClassFixture<ApplicationFactory<Program>>
     [Fact]
     public async Task UsersCanBeQueried()
     {
-        async Task<string> CreateUser(UserDto user)
-        {
-            var response = await _client.PostAsJsonAsync("/api/users", user);
-            var content = await response.Content.ReadFromJsonAsync<UserDto>();
-            return content!.Id!;
-        }
-
         var users = new Faker<UserDto>()
             .RuleFor(x => x.Name, f => f.Person.FullName)
             .RuleFor(x => x.Email, (f, x) => f.Internet.Email(x.Name))
@@ -62,10 +55,24 @@ public class UsersFeatureTests : IClassFixture<ApplicationFactory<Program>>
     [Fact]
     public async Task UsersCanBeDeleted()
     {
+        var users = new Faker<UserDto>()
+            .RuleFor(x => x.Name, f => f.Person.FullName)
+            .RuleFor(x => x.Email, (f, x) => f.Internet.Email(x.Name))
+            .Generate(10);
+
+        await Task.WhenAll(users.Select(CreateUser));
+
         var response = await _client.GetAsync("/api/users");
         var existingUsers = await response.Content.ReadFromJsonAsync<IList<UserDto>>();
 
         var deleteResponse = await _client.DeleteAsync($"/api/users/{existingUsers!.First().Id}");
         deleteResponse.StatusCode.Should().Be(HttpStatusCode.OK);
+    }
+
+    private async Task<string> CreateUser(UserDto user)
+    {
+        var response = await _client.PostAsJsonAsync("/api/users", user);
+        var content = await response.Content.ReadFromJsonAsync<UserDto>();
+        return content!.Id!;
     }
 }
